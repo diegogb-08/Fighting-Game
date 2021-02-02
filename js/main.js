1ab61d79d6c4dbf61f3b74bf38e5da9d7d10abe3
Diego <d.garciabrisa@gmail.com>
2021-02-02 13:57:34 +0100

    Music out (Comment)

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
-let audio = document.getElementById('audio');
-let count = 0;
+// let audio = document.getElementById('audio');
+// let count = 0;
 
-const playPause = () => {
-    if(count == 0) {
-        count = 1;
-        audio.play();
-    }else{
-        count = 0;
-        audio.pause();
-    }
-};
+// const playPause = () => {
+//     if(count == 0) {
+//         count = 1;
+//         audio.play();
+//     }else{
+//         count = 0;
+//         audio.pause();
+//     }
+// };
 
 // const playPause = () => {
 //     if(audio.paused) {
