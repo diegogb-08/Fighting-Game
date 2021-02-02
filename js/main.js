d9f9ed01c4f0f3d40bddb7817478531e5e3c1b19
Diego <d.garciabrisa@gmail.com>
2021-02-02 16:51:16 +0100

    player selection added

--- js/main.js
+++ js/main.js
@@ -1,20 +1,55 @@
-// let audio = document.getElementById('audio');
-// let count = 0;
+// Change first screen (Start Game)
 
-// const playPause = () => {
-//     if(count == 0) {
-//         count = 1;
-//         audio.play();
-//     }else{
-//         count = 0;
-//         audio.pause();
-//     }
-// };
 
-// const playPause = () => {
-//     if(audio.paused) {
-//         audio.pause();
-//     }else{
-//         audio.play();
-//     };
-// };
+
+const changeScreen = (pastPhase,newPhase) => {
+    
+    let screen1 = document.getElementById(pastPhase);
+    let screen2 = document.getElementById(newPhase);
+
+    
+    
+    screen1.style.display = "none";
+    screen2.style.display = "flex";
+
+   
+};
+
+// resolveIn(3000).then(delay => {
+
+//     changeScreen(pastPhase,newPhase);
+    
+// });
+
+// VARIABLES
+
+let player1 = "";
+
+let player2 = "";
+
+
+// SELECT CHARACTER
+
+let selectCharacter = (character) => {
+    if(player1 == "") {
+        player1 = character;
+
+        
+        document.getElementById(character).className = "fighterSelected1";
+        document.getElementById(character).onclick = "";
+
+        let mensaje = document.getElementById("infoPlayer1");
+
+        mensaje.innerHTML = `You have chosen <br> ${player1}`;
+
+    }else{
+        player2 = character;
+
+        document.getElementById(character).className = "fighterSelected2";
+        document.getElementById(character).onclick = "";
+
+        let mensaje = document.getElementById("infoPlayer2");
+
+        mensaje.innerHTML = `You have chosen <br> ${player2}`;
+    };
+};
