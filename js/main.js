20a1b5d4713dd566db0fd682662c34116f27f66a
Diego <d.garciabrisa@gmail.com>
2021-02-02 19:26:41 +0100

    Fase3 added with selected fighters

--- js/main.js
+++ js/main.js
@@ -12,20 +12,33 @@ const changeScreen = (pastPhase,newPhase) => {
     screen1.style.display = "none";
     screen2.style.display = "flex";
 
-   
+    /*resolveIn(4000).then(delay => {
+
+        changeScreen("fase1","fase2");
+    }); */
 };
 
-// resolveIn(3000).then(delay => {
 
-//     changeScreen(pastPhase,newPhase);
-    
-// });
+
+
 
 // VARIABLES
 
 let player1 = "";
 
 let player2 = "";
+
+let cambiaPantalla = (faseAhora,faseFutura) => {
+    let pantallaActual = document.getElementById(faseAhora);
+
+    let pantallaDestino = document.getElementById(faseFutura);
+
+    //aqui procedemos con el cambio
+
+    pantallaActual.style.display = "none";
+    pantallaDestino.style.display = "flex";
+};
+
 
 
 // SELECT CHARACTER
@@ -51,5 +64,28 @@ let selectCharacter = (character) => {
         let mensaje = document.getElementById("infoPlayer2");
 
         mensaje.innerHTML = `You have chosen <br> ${player2}`;
+
+        // LOAD CHARACTERS INTO THE ARENA
+
+        let showPlayer1 = document.getElementById("opponent1");
+        let showPlayer2 = document.getElementById("opponent2");
+
+        showPlayer1.innerHTML = `<img id="opponentStyle" src="img/figthers/${player1}.png">`;
+        showPlayer2.innerHTML = `<img id="opponentStyle" src="img/figthers/${player2}.png">`;
+
+
+        // CHANGE SCREEN TO ARENA
+
+        resolveIn(3000).then(delay => {
+
+            cambiaPantalla("fase2","fase3");
+            
+        });
     };
 };
+
+//funcion de delay...
+
+const resolveIn = delay =>
+new Promise(res => setTimeout(() => res(delay), delay));
+
