d02cd5c31dab1da14694ededd716df442b3fdf50
Diego <d.garciabrisa@gmail.com>
2021-02-03 20:04:12 +0100

    3 attack functions working & Attack comments in showtime added

--- js/main.js
+++ js/main.js
@@ -8,35 +8,35 @@ class Fighter {
         this.strenght = strenght;
         this.defense = defense;
         this.luck = luck;
-        this.handicap = luck - Math.floor(Math.random() * 10);
+        this.handicap = luck - Math.floor(Math.random() * 5);
         this.power = power;
     };
 
     attack(enemy) {
-        enemy.life -= (this.strenght - enemy.defense) * (this.suerte - this.handicap);
+        enemy.life -= (this.strenght - enemy.defense/1.5) + (this.luck - this.handicap);
     };
 
     powerAttack(enemy) {
-        enemy.life -= (this.strenght + this.power) - (enemy.defense * this.power);
+        enemy.life -= (this.strenght + this.power) - enemy.defense;
     };
 
-    strikeBack() {
-        enemy.life -= (this.strenght + this.power) - (enemy.defense * this.power) + this.luck;
+    strikeBack(enemy) {
+        enemy.life -= (this.strenght + this.power) - (enemy.defense - this.power) + this.luck;
     };
 };
 
 
 //Instancias y variables globales
-//nombre,vida,fuerza,defensa,suerte
+// name,  life,   strenght,   defense,  luck,  power
 
-let fighter1 = new Fighter("C-18",300,30,50,7,30);
-let fighter2 = new Fighter("Buu",300,35,45,3,45);
-let fighter3 = new Fighter("Cell",300,40,25,6,40);
-let fighter4 = new Fighter("Freeza",300,50,40,5,50);
-let fighter5 = new Fighter("Burter",300,30,30,2,35);
-let fighter6 = new Fighter("Krillin",300,35,45,2,20);
-let fighter7 = new Fighter("Goku",300,55,45,6,60);
-let fighter8 = new Fighter("Picolo",350,42,32,7,30);
+let fighter1 = new Fighter("C-18",300,33,48,7,20);
+let fighter2 = new Fighter("Buu",300,34,50,6,35);
+let fighter3 = new Fighter("Cell",300,40,25,6,38);
+let fighter4 = new Fighter("Freeza",300,51,38,7,44);
+let fighter5 = new Fighter("Burter",300,30,30,5,15);
+let fighter6 = new Fighter("Krillin",300,36,45,6,23);
+let fighter7 = new Fighter("Goku",300,55,45,6,58);
+let fighter8 = new Fighter("Picolo",350,42,32,5,31);
 let fighter9 = new Fighter("Trunks",300,45,50,8,35);
 let fighter10 = new Fighter("Vegeta",300,54,44,8,50);
 
@@ -59,22 +59,6 @@ let allplayers = {
 };
 
 
-
-//Funciones
-
-let startGame = () => {
-
-    let initialLife = 300;
-
-    player1.life = initialLife;
-    player2.life = initialLife;
-
-    player1 = "";
-
-    player2 = "";
-
-};
-
 // Change first screen 
 
 let changeScreen = (pastPhase,newPhase) => {
@@ -96,18 +80,17 @@ let selectCharacter = (character) => {
     if(player1 == "") {
         player1 = allplayers[character];
         
-
-        
         document.getElementById(character).className = "fighterSelected1";
         document.getElementById(character).onclick = "";
 
-        let mensaje = document.getElementById("infoPlayer1");
+        let mensaje1 = document.getElementById("infoPlayer1");
+        let infoFighter1 = document.getElementById("infoFighter1");
 
-        mensaje.innerHTML = `You have chosen <br> ${player1.name}`;
-
-        let life = document.getElementById("liveP1")
-
-        life.innerHTML = `${player1.life}`;
+        mensaje1.innerHTML = `You have chosen <br> ${player1.name}`;
+        infoFighter1.innerHTML = `Strenght:  ${player1.strenght} <br>
+                                Defense:  ${player1.defense} <br> 
+                                Luck:  ${player1.luck} <br>
+                                Power:  ${player1.power}`;
 
     }else{
         player2 = allplayers[character];
@@ -115,13 +98,14 @@ let selectCharacter = (character) => {
         document.getElementById(character).className = "fighterSelected2";
         document.getElementById(character).onclick = "";
 
-        let mensaje = document.getElementById("infoPlayer2");
+        let mensaje2 = document.getElementById("infoPlayer2");
+        let infoFighter2 = document.getElementById("infoFighter2");
 
-        mensaje.innerHTML = `You have chosen <br> ${player2.name}`;
-
-        let life = document.getElementById("liveP2")
-
-        life.innerHTML = `${player2.life}`;
+        mensaje2.innerHTML = `You have chosen <br> ${player2.name}`;
+        infoFighter2.innerHTML = `${player1.strenght}  :Strenght<br>
+                                   ${player2.defense}  :Defense<br> 
+                                    ${player2.luck}  :Luck<br>
+                                    ${player2.power}  :Power`;
 
         // LOAD CHARACTERS INTO THE ARENA
 
@@ -129,16 +113,19 @@ let selectCharacter = (character) => {
         let showPlayer2 = document.getElementById("opponent2");
         let showNameP1 = document.getElementById("fighterName1")
         let showNameP2 = document.getElementById("fighterName2")
+        let showLife1 = document.getElementById("liveP1")
+        let showLife2 = document.getElementById("liveP2")
 
         showPlayer1.innerHTML = `<img id="opponentStyle" src="img/figthers/${player1.name}.png">`;
         showPlayer2.innerHTML = `<img id="opponentStyle" src="img/figthers/${player2.name}.png">`;
         showNameP1.innerHTML = `${player1.name}`;
         showNameP2.innerHTML = `${player2.name}`;
-
+        showLife1.innerHTML = `${player1.life}`;
+        showLife2.innerHTML = `${player2.life}`;
 
         // CHANGE SCREEN TO ARENA
 
-        resolveIn(3000).then(delay => {
+        resolveIn(4000).then(delay => {
 
             changeScreen("fase2","fase3");
             
@@ -156,33 +143,47 @@ const hit = () => {
 
     let turn = Math.floor(Math.random() * 2);
     let specialAttack = Math.floor(Math.random() * 10);
+    let infoPowerAttack = document.getElementById("infoBattle");
+
 
     if(turn == 0) {
         if(specialAttack > 3 && specialAttack < 7) {
-            // if(specialAttack == 5){
-            //     player2.strikeBack(player1);
-            //     console.log("StrikeBack player2");
-            // };
-            player1.powerAttack(player2);
-            console.log("ATAQUE ESPECIAL player 1 " + parseInt(player2.life));
-        };
-        player1.attack(player2);
-        console.log("Attack player 1 " + parseInt(player2.life));
+            if(specialAttack == 5 || specialAttack == player2.luck){
+                console.log("STRIKEBACK PLAYER 2")
+                player2.strikeBack(player1);
+                infoPowerAttack.innerHTML = `${player1.name} SUPER attacks but ${player2.name} StrikeBack`;
+            }else{
+                infoPowerAttack.innerHTML = `${player1.name} SUPER attack`;
+                player1.powerAttack(player2);
+            };
+        }else {
+            player1.attack(player2);
+            infoPowerAttack.innerHTML = `${player1.name} attacks`;
+        };  
     }else{
         if(specialAttack > 3 && specialAttack < 7) {
-            // if(specialAttack == 5) {
-            //     player1.strikeBack(player2);
-            //     console.log("StrikeBack player1");
-            // };
-            player2.powerAttack(player1);
-            console.log("ATAQUE ESPECIAL player 2 " + parseInt(player1.life));
-        };
-        player2.attack(player1);
-        console.log("Attack player 2 " + parseInt(player1.life));
+            if(specialAttack == 5 || specialAttack == player1.luck) {
+                console.log("STRIKEBACK PLAYER 1")
+                player1.strikeBack(player2);
+                infoPowerAttack.innerHTML = `${player2.name} SUPER attack but ${player1.name} StrikeBack`;
+            }else{
+                infoPowerAttack.innerHTML = `${player2.name} SUPER attack`;
+                player2.powerAttack(player1);
+            };
+        }else{
+            player2.attack(player1);
+            infoPowerAttack.innerHTML = `${player2.name} attacks`;
+        }
     };
 
-    // console.log(player1.name + " " + player1.life);
-    // console.log(player2.name + " " + player2.life);
+
+    // FIGHTERS LIFE COUNTING
+
+        let life1 = document.getElementById("liveP1")
+        let life2 = document.getElementById("liveP2")
+
+        life1.innerHTML = Math.floor(`${player1.life}`);
+        life2.innerHTML = Math.floor(`${player2.life}`);
 
 };
 
