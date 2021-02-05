87b002128e7a21363688f225bbe4c3b4dc80d15e
Diego <d.garciabrisa@gmail.com>
2021-02-05 12:08:31 +0100

    buttons added to phase 4

---
+++ js/keyboard.js
@@ -0,0 +1,18 @@
+let key_left = 37;
+let key_right = 39;
+let key_up = 38;
+let key_down = 40;
+
+const initializeKeyboard = () => {
+
+    $(document).keydown(function(e) {
+
+        if (e.keyCode == key_left) {
+
+        }
+        if (e.keyCode == key_left) {
+            
+        }
+
+    });
+};
